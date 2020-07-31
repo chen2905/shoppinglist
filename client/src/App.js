74d3552c4e1bfbeb43c479e629693702c67e39e6
Chen Gao <maxlin2905@gmail.com>
2020-07-31 16:59:14 +1000

    459

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppNavbar from './components/AppNavbar'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import ShoppingList from './components/ShoppingList'
+function App() {
+  return (
+    <div className="App">
+     <AppNavbar/>
+     <ShoppingList/>
+    </div>
+  );
+}
+
+export default App;
